724edc824edb412fbee5f171b15f7782296d67f6
OMGDuke <lex1989@gmail.com>
2017-07-16 13:09:48 +0100

    Added functionality to create a todo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
   </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
 
 export default {
   name: 'app',
   components: {
     TodoList,
+    CreateTodo,
   },
   data() {
     return {
@@ -32,6 +35,11 @@ export default {
         done: false,
       }],
     };
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
   },
 };
 </script>
